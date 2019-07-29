de6aac15062350d1f4ed4686bb95723b4df8bb8d
Ishnoor-Singh <ishnoors@gmail.com>
2019-07-29 11:23:26 -0700

    basic frontend almost ready

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './Login';
+import InboxView from './InboxView';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    withRouter
+  } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/inbox/" component={InboxView} />
+      </Switch>
+    </Router>
+    , document.getElementById('root'));
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
