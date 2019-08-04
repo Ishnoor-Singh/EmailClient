c5fbfe2ab44821c8eeb7dddbb7e74c56c52a91fd
Ishnoor-Singh <ishnoors@gmail.com>
2019-08-04 04:12:31 -0700

    backend ready forr api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello world")
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({"reponse" : 400});
+})
+
+app.listen (PORT);
